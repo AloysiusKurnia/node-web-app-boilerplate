bcb3a8696e3cd2c0322037c045bdca75a6772dbf
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2024-05-21 23:33:39 +0700

    prisma initialization

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+
 
 const app = express();
 const port = 3000;
 
+const prisma = new PrismaClient();
+
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
