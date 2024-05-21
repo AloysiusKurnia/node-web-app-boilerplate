70d0ddd55a9e2ea752d8ebe927308df900feaefe
Aloysius Kurnia Mahendra <aloysius1234@yahoo.co.id>
2024-05-22 05:53:13 +0700

    prisma example

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,17 @@ const port = 3000;
 
 const prisma = new PrismaClient();
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+app.set('views', './src/views');
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: 'Hello world', message: `I'm using Pug` });
+});
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany({
+        select: { name: true, email: true }
+    });
+    res.render('users', { users });
 });
 
 app.listen(port, () => {
